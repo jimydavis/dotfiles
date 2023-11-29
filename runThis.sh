7a5b0967a9db702ed08537291e2c0c61da62db8e
mschulz <lt.thet@gmail.com>
2023-11-29 10:55:13 +0800

    Update runThis.sh

--- runThis.sh
+++ runThis.sh
@@ -1,5 +1,12 @@
 apt update
-apt install unzip
+apt install unzip -y
+apt install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt update
+apt install nodejs -y
+bash /tmp/nodesource_setup.sh
 git clone https://github.com/jimydavis/dotfiles
 cd dotfiles
 dpkg -i ripgrep_13.0.0_amd64.deb

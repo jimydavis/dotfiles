7ff9e6c78feb003a26c638ccc9d9165b085592d9
mschulz <lt.thet@gmail.com>
2023-11-29 10:26:12 +0800

    Update runThis.sh

--- runThis.sh
+++ runThis.sh
@@ -2,7 +2,7 @@ apt update
 apt install unzip
 git clone https://github.com/jimydavis/dotfiles
 cd dotfiles
-sudo dpkg -i ripgrep_13.0.0_amd64.deb
+dpkg -i ripgrep_13.0.0_amd64.deb
 ./nvim.appimage --appimage-extract
 ln -s $(pwd)/squashfs-root/AppRun /usr/bin/nvim
 mkdir ~/.config

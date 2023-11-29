e8e65c63efcbf0d6a9b63e63efeef600c1e4da6f
mschulz <lt.thet@gmail.com>
2023-11-29 10:13:24 +0800

    Create runThis.sh

---
+++ runThis.sh
@@ -0,0 +1,7 @@
+apt update
+git clone https://github.com/jimydavis/dotfiles
+cd dotfiles
+./nvim.appimage --appimage-extract
+ln -s $(pwd)/squashfs-root/AppRun /usr/bin/nvim
+mkdir ~/.config
+cp -a nvim ~/.config

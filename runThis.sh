3cb35b944444671bfdacf86a075ec39aa06a0950
mschulz <lt.thet@gmail.com>
2023-11-30 10:09:04 +0800

    Update runThis.sh

--- runThis.sh
+++ runThis.sh
@@ -6,9 +6,6 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt update
 apt install nodejs -y
-bash /tmp/nodesource_setup.sh
-git clone https://github.com/jimydavis/dotfiles
-cd dotfiles
 dpkg -i ripgrep_13.0.0_amd64.deb
 ln -s $(pwd)/nvim-squashfs/AppRun /usr/bin/nvim
 mkdir ~/.config
